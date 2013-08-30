68ff5eb95996c76301bd0c4c5b8e6b616254c970
Alexey Novikov <velocityzen@gmail.com>
2013-08-30 20:38:18 +0400

    new slider

--- js/index.js
+++ js/index.js
@@ -1,61 +1,43 @@
 $(document).ready(function() {
 	var bubble = function(selector) {
-			$(selector).addClass("active");	
+			$(selector).addClass("active");
 		},
-		phone = function(num) {console.log("phone", num)}, 
+		phone = function(num) {console.log("phone", num)},
 		slides = [
-			{
-				children: 0,
-				logical: 0
-			},
+			{},
 			{
 				children: 3,
 				cb: function() {
 					bubble(".bubble.left");
-				},
-				logical: 1
+				}
 			},
 			{
 				cb: function() {
 					bubble(".bubble.right");
-				},
-				parent: 1
+				}
 			},
 			{
 				cb: function() {
 					bubble(".bubble.bottom");
-				},
-				parent: 1
+				}
 			},
-			{
-				parent: 1
-			},
+			{},
 			{
 				children: 4,
-				cb: phone,
-				logical: 2
+				scrollOnEnd: true,
+				cb: phone
 			},
 			{
-				cb: phone,
-				parent: 2
+				cb: phone
 			},
 			{
-				cb: phone,
-				parent: 2
+				cb: phone
 			},
 			{
-				cb: phone,
-				parent: 2
+				cb: phone
 			},
-			{
-				wontFix: true,
-				parent: 2
-			},
-			{
-				children: 0,
-				logical: 3
-			}
-	]
+			{}
+	];
 
 	new WindowSlider("section", slides);
 })
--- js/windowSlider.js
+++ js/windowSlider.js
@@ -1,70 +1,90 @@
+/*jshint strict: false, browser:true, jquery: true*/
+var $w = $(window),
+	$d = $(document);
+
 var WindowSlider = function(selector, slideDesc) {
 	var self = this;
 
-	self.$w = $(window);
-	self.$sections = $(selector);
-	self.slideDesc = slideDesc; 
+	self.slideDesc = slideDesc;
 	self.lastF = 0;
 	self.h = null;
+	self.dh = null;
 
-	self.$w
+	var slideNum = 0,
+		skip = 0,
+		$slides = $(selector);
+
+	for(var i in slideDesc) {
+		var slide = slideDesc[i];
+
+		if(slide.children || skip <= 0) {
+			slide.children ? skip = slide.children + 1 : false;
+			slide.$b = $slides.eq(slideNum);
+			slideNum++;
+		}
+
+		skip--;
+	}
+
+	$w
 		.on("resize", function() {
-			self.h = self.$w.height();
+			self.h = $w.height();
+			self.dh = $d.height();
 			self.manageSlideFixation(self.lastF);
 		})
 		.on("scroll", function() {
-			var s = self.$w.scrollTop(),
+			var s = $w.scrollTop(),
 				curF = Math.floor(s / self.h);
 
-			if (curF === self.lastF) {
-				return;
+			if(curF >= 0 && curF !== self.lastF) {
+				self.manageSlideFixation(curF);
+				self.lastF = curF;
 			}
-
-			self.manageSlideFixation(curF);
-			self.lastF = curF;
 		})
 		.trigger("resize");
-}
+};
 
-WindowSlider.prototype.manageSlideFixation = function (fixer) {
-	var topPrev = 0,
-		curSlide = this.slideDesc[fixer],
-		limiter = curSlide.logical != undefined ? curSlide.logical : curSlide.parent;
+WindowSlider.prototype.manageSlideFixation = function (index) {
+	var self = this,
+		k;
 
-	if (curSlide.wontFix) {
-		var top = parseInt(this.$sections.get(limiter + 1).style["top"]) - this.h;
-	
-		this.setSlideTop(limiter, top);
-		this.setSlideTop(limiter + 1, top);
-	} else {
-		for (var i in this.slideDesc) {
-			var slide = this.slideDesc[i];
+	var currSlide = this.slideDesc[index];
 
-			if (slide.hasOwnProperty("logical")) {
-				if (slide.logical <= limiter) {
-					this.fixSlide(slide.logical);
-				} else {
-					this.setSlideTop(slide.logical, topPrev);
-				}
-				topPrev += this.h * (slide.children + 1);
+	if(!currSlide.$b) {
+		k = $w.scrollTop() / self.dh;
+	}
+
+	for(var i in this.slideDesc) {
+		var slide = this.slideDesc[i];
+
+		if(slide.$b) {
+			if(i <= index && slide.fixed !== false) {
+				slide.$b.css({
+					top: 0,
+					position: "fixed"
+				});
+			}
+
+			if(i > index && !slide.fixed) {
+				slide.$b.css({
+					top: i * this.h + "px",
+					position: ""
+				});
 			}
 		}
 	}
 
-	if (curSlide.cb != undefined) {
-		setTimeout(function() {
-			curSlide.cb();
-		}, 50);
-		
+	if(!currSlide.$b) {
+		self.slideDesc.splice(index, 1);
+		self.dh = $d.height();
+		$w.scrollTop(self.dh * k);
 	}
-}
 
-WindowSlider.prototype.fixSlide = function(slide) {
-	this.$sections.get(slide).style["top"] = 0;
-	this.$sections.get(slide).style["position"] = "fixed";	
-}
+	console.log(self.slideDesc.length);
 
-WindowSlider.prototype.setSlideTop = function(slide, top) {
-	this.$sections.get(slide).style["top"] = top + "px";
-	this.$sections.get(slide).style["position"] = "";
-}
+	if(currSlide.cb !== undefined) {
+		setTimeout(function() {
+			currSlide.cb();
+		}, 100);
+	}
+};
